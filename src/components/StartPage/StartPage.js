73c979064fb42610548142c33d3732fa20e5266d
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2022-08-19 19:25:31 +0530

    Fix startpage heading and description contrast-ratio problem on dark mode

--- src/components/StartPage/StartPage.js
+++ src/components/StartPage/StartPage.js
@@ -2,7 +2,7 @@ import QuizButton from "../QuizButton/QuizButton";
 
 function StartPage({ hideStartPage }) {
   return (
-    <div data-testid="start-page" className="text-center text-blue-dark">
+    <div data-testid="start-page" className="text-center text-blue-dark dark:text-white">
       <h1 className="text-4xl font-semibold tracking-wide">Trivia Quiz</h1>
       <p className="mt-1 mb-7">your favorite quiz app</p>
       <QuizButton text="Start Quiz" handleClick={hideStartPage} />
