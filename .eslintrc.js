f57353a2e184624873d9920dc25d2a28861e0177
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-01-13 20:06:07 +0530

    chore: configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "react-app",
+    "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "no-console": "warn",
+    "import/export": "off",
+    "import/first": "error",
+    "import/newline-after-import": ["error", { count: 1 }],
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+        },
+      },
+    ],
+  },
+};
