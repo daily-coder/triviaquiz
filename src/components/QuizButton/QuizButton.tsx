4ebdf569da56ac81edbcaa0281dda42f101e5560
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-01-29 18:41:45 +0530

    feat: migrate QuizButton.js to typescript

--- src/components/QuizButton/QuizButton.tsx
+++ src/components/QuizButton/QuizButton.tsx
@@ -1,4 +1,9 @@
-function QuizButton({ text, handleClick }) {
+interface QuizButtonProps {
+  text: string;
+  handleClick(): void;
+}
+
+function QuizButton({ text, handleClick }: QuizButtonProps) {
   return (
     <button
       className="justify-self-center ml-3 p-4 w-48 text-white text-lg font-bold rounded-2xl bg-blue-dark text-md hover:bg-blue-900"
