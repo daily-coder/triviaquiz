bae1c75bd03fada68394f2d7f062b67dd5e82f8b
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-01-29 18:01:28 +0530

    feat: migrate decode.js to typescript

--- src/helper/decode/decode.ts
+++ src/helper/decode/decode.ts
@@ -1,21 +1,22 @@
-import he from "he";
+import he, { Decode } from "he";
 
-function getCopy(value, getValue) {
+function getCopy(value: unknown, getValue: Decode) {
   if (value == null) {
     return value;
   } else if (typeof value !== "object") {
     return getValue(String(value)); // convert non-string values to strings
   } else {
     const keys = Object.keys(value);
-    const copy = Array.isArray(value) ? [] : {};
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const copy: Record<string, any> = Array.isArray(value) ? [] : {};
     for (const key of keys) {
-      copy[key] = getCopy(value[key], getValue);
+      copy[key] = getCopy(value[key as keyof typeof value], getValue);
     }
     return copy;
   }
 }
 
-function decodeHTML(obj) {
+function decodeHTML(obj: unknown) {
   return getCopy(obj, he.decode);
 }
 
