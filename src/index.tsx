41f5c7624ec0277d679b0ea18eaff4f8f348c04a
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-01-31 19:27:15 +0530

    feat: migrate index.js to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  throw new Error("Root Element is not found");
+}
+
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
