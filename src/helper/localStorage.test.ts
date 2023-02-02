abd535b2727199db27a08b17cba87713aca5081a
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-02-02 19:11:03 +0530

    refactor(test): create setup function for shared logic

--- src/helper/localStorage.test.ts
+++ src/helper/localStorage.test.ts
@@ -4,11 +4,15 @@ const temp = window.localStorage;
 beforeEach(() => localStorage.clear());
 afterEach(() => (window.localStorage = temp));
 
-test("store data in localStorage", () => {
+function setup() {
   const key = "token";
   const value = Math.random();
   saveState(key, value);
+  return { key, value };
+}
 
+test("store data in localStorage", () => {
+  const { key, value } = setup();
   const serializedData = localStorage.getItem(key);
   if (!serializedData) {
     throw new Error("Failed to store data in localStorage");
@@ -17,17 +21,12 @@ test("store data in localStorage", () => {
 });
 
 test("retrieve data from localStorage", () => {
-  const key = "token";
-  const value = Math.random();
-  saveState(key, value);
-
+  const { key, value } = setup();
   expect(loadState(key)).toBe(value);
 });
 
 test("return undefined if there is an error while retrieving", () => {
-  const key = "token";
-  const value = Math.random();
-  saveState(key, value);
+  const { key } = setup();
 
   // create an error by deleting localStorage
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
